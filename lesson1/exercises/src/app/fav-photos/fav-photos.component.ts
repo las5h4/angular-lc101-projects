7a3403761c44262a088ffdf651cd4df80ea84eef
las5h4 <loganasmalley@gmail.com>
2019-12-15 18:40:50 -0600

    edited css for movie list, chore list, and favorite pics components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://i.redd.it/j9erfsz3ps441.jpg';
+  image2 = 'https://preview.redd.it/cn7uezlb6u441.jpg?width=640&crop=smart&auto=webp&s=16f7c3e49d3acb0c46d0d24cc24114bed9cb5a00';
+  image3 = 'https://preview.redd.it/t8u1lqacvr441.jpg?width=640&crop=smart&auto=webp&s=5689e45285b19773d644db7640ce037f72537f2d';
 
   constructor() { }
 
